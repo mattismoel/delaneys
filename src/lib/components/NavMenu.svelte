ec59aaa5e503b85c21c1684a7a5b5e5086d718a3
Mattis Kristensen <mattismoel@gmail.com>
2025-09-26 15:44:36 +0200

    attempt fix of navmenu

--- src/lib/components/NavMenu.svelte
+++ src/lib/components/NavMenu.svelte
@@ -19,22 +19,22 @@
     class="z-50 absolute top-0 left-0 h-full w-full bg-[black]/50 opacity-0 transition-opacity group-[.open]:opacity-100 group-[.open]:pointer-events-auto"
   ></div>
 
-  <div
-    class="z-50 absolute bottom-0 left-0 w-full translate-y-full px-responsive transition-transform group-[.open]:translate-y-0"
+  <aside
+    class="fixed bottom-0 z-50 block w-screen translate-y-full px-responsive transition-transform group-[.open]:translate-y-0"
   >
     <div
-      class="pointer-events-auto w-full bg-background-100 border border-border px-8 pt-8 pb-32 rounded-t-sm"
+      class="w-full h-full bg-background-100 border border-border px-8 pt-8 pb-32 rounded-t-sm"
       use:clickOutside
       onclickoutside={onClose}
     >
-      <ul class="flex flex-col gap-2 text-2xl font-semibold text-text-dark/80">
+      <ul class="flex flex-col gap-3 text-3xl font-semibold text-text-dark/80">
         {@render entry("/", "Hjem", onClose)}
         {@render entry("/menu", "Menu", onClose)}
         {@render entry("/om-os", "Om os", onClose)}
         {@render entry("/kontakt", "Kontakt", onClose)}
       </ul>
     </div>
-  </div>
+  </aside>
 </div>
 
 {#snippet entry(pathname: string, title: string, onClose: () => void)}
