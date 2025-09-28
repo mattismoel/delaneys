e04f27eadc05e5836d8a8764b936f42c38d30a0e
Mattis Møl Kristensen <mattismoel@gmail.com>
2025-09-28 14:00:30 +0200

    vite server https
    >

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { readFileSync } from "node:fs"
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		https: {
+			key: readFileSync(`${__dirname}/cert/key.pem`),
+			cert: readFileSync(`${__dirname}/cert/cert.pem`)
+		},
+		proxy: {}
+	}
 });
