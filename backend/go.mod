1d88b1d167e2271171fce4afa00fbc3fd2ac68f2
Mattis Møl Kristensen <mattismoel@gmail.com>
2025-10-07 19:48:54 +0200

    initialise project

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/mattismoel/delaneys/backend
+
+go 1.24.0
---
+++ frontend/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+	server: {
+		host: "0.0.0.0",
+	},
+	plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
+		tailwindcss(),
+		react({
+			babel: {
+				plugins: [['babel-plugin-react-compiler']],
+			},
+		}),
+	],
+})
