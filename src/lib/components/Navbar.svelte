f3256a20e857a59d21954319b5e53165f30a920b
Mattis Kristensen <mattismoel@gmail.com>
2025-11-06 00:51:53 +0100

    fix centering of nav menu button

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -37,7 +37,7 @@
 
   <button
     onclick={() => (isMenuShown = true)}
-    class="sm:hidden"
+    class="flex h-8 items-center justify-center sm:hidden"
     aria-label="Menu button"
   >
     <span class="icon-[lucide--menu]"></span>
