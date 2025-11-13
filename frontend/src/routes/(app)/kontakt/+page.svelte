91305c12b3f8220ae72c43a0e2ebe2d5b439a860
Mattis Kristensen <mattismoel@gmail.com>
2025-11-13 15:49:47 +0100

    update contact page

--- frontend/src/routes/(app)/kontakt/+page.svelte
+++ frontend/src/routes/(app)/kontakt/+page.svelte
@@ -1,40 +1,46 @@
 <script lang="ts">
   import Accordion from "$lib/components/Accordion.svelte";
   import InlineLink from "$lib/components/InlineLink.svelte";
-  import Map from "$lib/components/Map.svelte";
+  // import Map from "$lib/components/Map.svelte";
 
   let { data } = $props();
 </script>
 
-<main class="flex min-h-svh flex-col gap-16 py-32">
-  <section class="mx-responsive">
-    <p class="leading-loose">
-      Har du spørgsmål, som ikke kan besvares af <InlineLink href="#faq"
-        >de oftest stillede spørgsmål</InlineLink
-      >, eller er du blot nysgerrig, så ring endelig på
-      <InlineLink href="tel:+4541109940">+45 41 10 99 40</InlineLink>, eller
-      skriv en mail på
-      <InlineLink href="mailto:info@delaneys.dk">info@delaneys.dk</InlineLink>.
-      <br />
-      <br />
-      Vi glæder os til at høre fra dig – eller endnu bedre, se dig i baren!
-    </p>
-  </section>
+{#snippet contactEntry(title: string, text: string, href: string | undefined)}
+  <div class="flex flex-col items-center">
+    <h3 class="font-serif font-bold">{title}</h3>
+    {#if href}
+      <a {href}>
+        {text}
+      </a>
+    {:else}
+      <span>{text}</span>
+    {/if}
+  </div>
+{/snippet}
 
-  <section class="mx-responsive w-full">
-    <h1 class="mb-4 font-serif text-2xl font-bold" id="faq">
+<main class="flex min-h-svh flex-col py-32">
+  <section class="mx-responsive pb-8">
+    <h1 class="mb-8 font-serif text-4xl font-bold">Kontakt os</h1>
+    <p class="mb-12 leading-loose text-text-dark-muted">
+      Hvis du er i tvivl om noget, eller blot er nysgerrig er du altid velkommen
+      til at tage fat i os på <InlineLink href="mailto:info@delaneys.dk"
+        >info@delaneys.dk</InlineLink
+      >, eller <InlineLink href="tel:+4541109940">+45 41 10 99 40</InlineLink>.
+      Da vi får mange henvendelser anbefaler vi først og fremmest at tage et kig
+      under
+      <i>ofte stillede spørgsmål</i>. Vi glæder os til at høre fra dig, eller
+      endnu bedre&nbsp;&mdash;&nbsp;se dig i baren!
+    </p>
+
+    <h2 class="mb-4 font-serif text-2xl font-bold" id="faq">
       Ofte stillede spørgsmål
-    </h1>
+    </h2>
 
     <div class="flex flex-col gap-2">
       {#each data.questions as question}
         <Accordion title={question.title} description={question.description} />
       {/each}
     </div>
-  </section>
-
-  <section class="mx-responsive w-full">
-    <h1 class="mb-4 font-serif text-2xl font-bold">Find os hér</h1>
-    <Map address="Tolderlundsvej 46" title="Delaney's' Bar & Bottleshop" />
   </section>
 </main>
