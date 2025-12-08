96d2ddc4344d739fc1860ac9d67d525d86769fa1
Mattis Kristensen <mattismoel@gmail.com>
2025-12-08 14:36:10 +0100

    update button styles

--- frontend/src/lib/components/Button.svelte
+++ frontend/src/lib/components/Button.svelte
@@ -4,7 +4,7 @@
     HTMLButtonAttributes,
   } from "svelte/elements";
 
-  type Variant = "primary" | "ghost" | "outline-dark";
+  type Variant = "primary" | "ghost" | "outline-dark" | "transparent";
 
   type BaseProps = {
     variant?: Variant;
@@ -27,13 +27,15 @@
   this={rest.href !== undefined ? "a" : "button"}
   {...rest}
   class={[
-    "flex items-center justify-center gap-2 rounded-lg border px-6 py-2 text-center transition-[background,filter]",
+    "flex items-center justify-center gap-2 rounded-lg border px-6 py-2 text-center transition-[background,border-color,filter] duration-100",
     variant === "primary" &&
       "border-border bg-primary font-medium text-text-dark hover:not-disabled:brightness-80 disabled:text-text-dark/50",
     variant === "ghost" &&
       "border-primary/15 bg-primary/10 text-text-light hover:bg-primary/15",
     variant === "outline-dark" &&
       "border-border font-medium hover:border-text-dark hover:bg-text-dark hover:text-text-light",
+    variant === "transparent" &&
+      "border-transparent hover:not-disabled:border-border hover:not-disabled:bg-surface-300 disabled:text-text-dark/50",
     rest.class,
   ]}
 >
