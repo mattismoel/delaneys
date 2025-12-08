e8722200b4e1ff24912c3aea3ec0be933207d701
Mattis Kristensen <mattismoel@gmail.com>
2025-12-08 14:39:43 +0100

    formatting

--- frontend/src/lib/services/pocketbase/auth.ts
+++ frontend/src/lib/services/pocketbase/auth.ts
@@ -1,49 +1,58 @@
-import { type AuthProvider, type CurrentUserHandler, type IsAuthenticatedHandler, type LoginHandler, type RegisterHandler, type RequestPasswordResetHandler, type SignOutHandler } from "$lib/features/auth/provider";
+import {
+  type AuthProvider,
+  type CurrentUserHandler,
+  type IsAuthenticatedHandler,
+  type LoginHandler,
+  type RegisterHandler,
+  type RequestPasswordResetHandler,
+  type SignOutHandler,
+} from "$lib/features/auth/provider";
 import { user } from "$lib/features/users/user";
-import PocketBase from "pocketbase"
+import PocketBase from "pocketbase";
 
 export const pocketBaseAuthProvider = (pb: PocketBase): AuthProvider => {
-	const login: LoginHandler = async (data) => {
-		await pb.collection("users").authWithPassword(data.email, data.password)
-	}
+  const login: LoginHandler = async (data) => {
+    await pb.collection("users").authWithPassword(data.email, data.password);
+  };
 
-	const register: RegisterHandler = async (data) => {
-		await pb.collection("users").create({ ...data, emailVisibility: true })
-		await pb.collection("users").requestVerification(data.email)
-	}
+  const register: RegisterHandler = async (data) => {
+    await pb.collection("users").create({ ...data, emailVisibility: true });
+    await pb.collection("users").requestVerification(data.email);
+  };
 
-	const signOut: SignOutHandler = async () => {
-		pb.authStore.clear()
-	}
+  const signOut: SignOutHandler = async () => {
+    pb.authStore.clear();
+  };
 
-	const isAuthenticated: IsAuthenticatedHandler = async () => {
-		if (!pb.authStore.isValid) return false
+  const isAuthenticated: IsAuthenticatedHandler = async () => {
+    if (!pb.authStore.isValid) return false;
 
-		try {
-			await pb.collection("users").authRefresh()
-			return true
-		} catch (_) {
-			pb.authStore.clear()
-			return false
-		}
-	}
+    try {
+      await pb.collection("users").authRefresh();
+      return true;
+    } catch (_) {
+      pb.authStore.clear();
+      return false;
+    }
+  };
 
-	const currentUser: CurrentUserHandler = async () => {
-		if (!pb.authStore.isValid) return null
-		const currentUser = user.parse(pb.authStore.record)
-		return currentUser
-	}
+  const currentUser: CurrentUserHandler = async () => {
+    if (!pb.authStore.isValid) return null;
+    const currentUser = user.parse(pb.authStore.record);
+    return currentUser;
+  };
 
-	const requestPasswordReset: RequestPasswordResetHandler = async (email) => {
-		await pb.collection("users").requestPasswordReset(email)
-	}
+  const requestPasswordReset: RequestPasswordResetHandler = async (email) => {
+    const sent = await pb.collection("users").requestPasswordReset(email);
+    if (!sent) throw new Error("Could not send email");
+  };
 
-	return {
-		login,
-		register,
-		signOut,
-		isAuthenticated,
-		currentUser,
-		requestPasswordReset
-	}
-}
+  return {
+    login,
+    register,
+    signOut,
+    isAuthenticated,
+    currentUser,
+    requestPasswordReset,
+  };
+};
