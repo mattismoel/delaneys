891c209307cd3693978d43a7940b398ef6d33699
Mattis Kristensen <mattismoel@gmail.com>
2025-12-08 12:57:07 +0100

    refactor auth

--- frontend/src/lib/features/auth/auth.remote.ts
+++ frontend/src/lib/features/auth/auth.remote.ts
@@ -1,3 +1,24 @@
+import { form, getRequestEvent, query } from "$app/server";
+import { redirect } from "@sveltejs/kit";
+import { loginForm, registerForm } from "./provider";
+
+export const isAuthenticated = query(async () => {
+  const { locals } = getRequestEvent();
+  return await locals.authProvider.isAuthenticated();
+});
+
+export const register = form(registerForm, async (data) => {
+  const { locals } = getRequestEvent();
+  await locals.authProvider.register(data);
+  redirect(303, `/auth/verification?email=${data.email}`);
+});
+
+export const login = form(loginForm, async (data) => {
+  const { locals } = getRequestEvent();
+  await locals.authProvider.login(data);
+  redirect(303, "/admin/dashboard");
+});
+
 export const getUsers = query(async () => {
   const { locals } = getRequestEvent();
   const users = await locals.userProvider.getUsers();
