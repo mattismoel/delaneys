d3618c4bee2146a690a679e1f92af140f2ea2d99
Mattis Kristensen <mattismoel@gmail.com>
2026-08-01 01:07:28 +0200

    temporarily add log

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -6,6 +6,15 @@ import { env } from "$env/dynamic/private"
 import { userSchema } from "$lib/features/users/user"
 
 export const handle: Handle = async ({ event, resolve }) => {
+  console.log({
+    locationId: env.UNTAPPD_LOCATION_ID,
+    menuId: env.UNTAPPD_MENU_ID,
+    // UNTAPPD_ENCODED_ACCESS_KEY=${{ secrets.UNTAPPD_ENCODED_ACCESS_KEY }}
+    // DATABASE_URL=${{ vars.DATABASE_URL }}
+    // UNTAPPD_LOCATION_ID=${{ vars.UNTAPPD_LOCATION_ID }}
+    // UNTAPPD_MENU_ID=${{ vars.UNTAPPD_MENU_ID }}
+
+  })
   event.locals.pocketbase = new PocketBase(env.DATABASE_URL)
   event.locals.pocketbase.authStore.loadFromCookie(event.request.headers.get("cookie") || "")
 
