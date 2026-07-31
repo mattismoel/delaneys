7fd6d9adfe20f1397694c22776d2beb1b4dbd0cc
Mattis Kristensen <mattismoel@gmail.com>
2026-08-01 01:07:28 +0200

    temporarily add log
    >
    
    >

--- frontend/src/lib/features/location/location.remote.ts
+++ frontend/src/lib/features/location/location.remote.ts
@@ -3,6 +3,7 @@ import { env } from "$env/dynamic/private";
 import { mapUntappdHoursResponse, mapUntappdMenuResponse, untappdFetch, untappdHoursResponse, untappdMenuResponse } from "./untappd";
 
 export const getMenu = query(async () => {
+  console.log(env.UNTAPPD_ENCODED_ACCESS_KEY)
   const menuResponse = await untappdFetch(`/menus/${env.UNTAPPD_MENU_ID}?full=true`)
   const menu = untappdMenuResponse.parse(menuResponse)
 
@@ -10,6 +11,7 @@ export const getMenu = query(async () => {
 });
 
 export const getHours = query(async () => {
+  console.log(env.UNTAPPD_ENCODED_ACCESS_KEY)
   const apiResponse = await untappdFetch(`/locations/${env.UNTAPPD_LOCATION_ID}/hours`)
   const response = untappdHoursResponse.parse(apiResponse)
 
